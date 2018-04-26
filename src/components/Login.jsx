d4abfbf13787819a7a0cc87afe71ebc0043fa7d6
Sunsern Cheamanunkul <sunsern@gmail.com>
2018-04-26 14:34:57 +0700

    dayu2

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,23 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { auth } from '../firebase';
 
+import { withStyles } from 'material-ui/styles';
+import Button from 'material-ui/Button';
+import Grid from 'material-ui/Grid';
+import Paper from 'material-ui/Paper';
+import TextField from 'material-ui/TextField';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+});
 
 class Login extends Component {
 
@@ -34,29 +51,41 @@ class Login extends Component {
 
     render() {
         const { email, password } = this.state;
+        const classes = this.props.classes;
         return (
-            <div>
-                <h1>Log in</h1>
-                <form onSubmit={this.onSubmit}>
-                <input
-                  value={email}
-                  onChange={this.handleChange('email')}
-                  type="text"
-                  placeholder="Email Address"
-                />
-                <input
-                  value={password}
-                  onChange={this.handleChange('password')}
-                  type="password"
-                  placeholder="Enter Password"
-                />
-                <button type="submit">
-                    Log in
-                </button>
-              </form>
-            </div>
+            <Grid container>
+                <Grid item xs={12}>
+                    <Paper className={classes.paper}>
+                        <h1>Log in</h1>
+                        <form onSubmit={this.onSubmit} autoComplete="off">
+                            <TextField
+                              id="email"
+                              label="Email"
+                              className={classes.textField}
+                              value={email}
+                              onChange={this.handleChange('email')}
+                              margin="normal"
+                              type="email"
+                            />
+                            <br />
+                            <TextField
+                              id="password"
+                              label="Password"
+                              className={classes.textField}
+                              value={password}
+                              onChange={this.handleChange('password')}
+                              margin="normal"
+                              type="password"
+                            />
+                            <br />
+                            <Button variant="raised" color="primary" type="submit">Log in</Button>
+                        </form>
+                        <p>Don't have an account? <Link to="/signup">Sign up here</Link></p>
+                    </Paper>
+                </Grid>
+            </Grid>
         );
     }
 }
 
-export default Login;
+export default withStyles(styles)(Login);
--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyAvpXhLswzgdfmFXnvSRrpdmhFWWfBZLow",
-    authDomain: "react-firebase-auth-83258.firebaseapp.com",
-    databaseURL: "https://react-firebase-auth-83258.firebaseio.com",
-    projectId: "react-firebase-auth-83258",
-    storageBucket: "react-firebase-auth-83258.appspot.com",
-    messagingSenderId: "612362768284"
+    apiKey: "AIzaSyDOhyQXZMYPxIDRhUL9R3ar5j-DnP8qlL0",
+    authDomain: "simple-note-31073.firebaseapp.com",
+    databaseURL: "https://simple-note-31073.firebaseio.com",
+    projectId: "simple-note-31073",
+    storageBucket: "simple-note-31073.appspot.com",
+    messagingSenderId: "668969723863"
 };
 
 firebase.initializeApp(config);
